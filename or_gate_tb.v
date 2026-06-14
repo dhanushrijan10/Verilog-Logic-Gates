3c225701c2ac54363a171c83056135f9d9f2af18
Dhanu Shrijan Boorlagadda <dhanushrijanboorlagadda@gmail.com>
2026-06-14 20:55:05 +0530

    Add OR gate project

---
+++ or_gate_tb.v
@@ -0,0 +1,29 @@
+module or_gate_tb;
+    reg A;
+    reg B;
+    wire Y;
+
+    or_gate uut (
+        .A(A),
+        .B(B),
+        .Y(Y)
+    );
+    initial begin
+        $dumpfile("wave.vcd");
+        $dumpvars(0, or_gate_tb);
+        $display("A B | Y");
+        A=0; B=0;
+        #10;
+        $display("%b %b | %b",A,B,Y);
+         A=0; B=1;
+        #10;
+        $display("%b %b | %b",A,B,Y);
+         A=1; B=0;
+        #10;
+        $display("%b %b | %b",A,B,Y);
+        A=1; B=1;
+        #10;
+        $display("%b %b | %b",A,B,Y);
+        $finish;
+    end
+endmodule
