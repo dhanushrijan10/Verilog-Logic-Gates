313088c149c1734e253c58fbe4385f73e992ffaa
Dhanu Shrijan Boorlagadda <dhanushrijanboorlagadda@gmail.com>
2026-06-14 12:11:29 +0530

    Add AND gate project

---
+++ and_gate_tb.v
@@ -0,0 +1,31 @@
+module and_gate_tb;
+    reg A;
+    reg B;
+    wire Y;
+
+    and_gate uut (
+        .A(A),
+        .B(B),
+        .Y(Y)
+    );
+    initial begin
+        $dumpfile("wave.vcd");
+        $dumpvars(0, and_gate_tb);
+        $display("A B | Y");
+
+        A=0; B=0;
+        #10;
+        $display("%b %b | %b",A,B,Y);
+        A=0; B=1;
+        #10;
+        $display("%b %b | %b",A,B,Y);
+        A=1; B=0;
+        #10;
+        $display("%b %b | %b",A,B,Y);
+        A=1; B=1;
+        #10;
+        $display("%b %b | %b",A,B,Y);
+
+        $finish;
+    end
+endmodule
