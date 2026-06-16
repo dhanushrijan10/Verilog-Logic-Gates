14829df4795696c21149528d836888ca0a9edb19
Dhanu Shrijan Boorlagadda <dhanushrijanboorlagadda@gmail.com>
2026-06-16 12:58:34 +0530

    Add XOR gate project

---
+++ xor_gate_tb.v
@@ -0,0 +1,33 @@
+module xor_gate_tb;
+    reg A;
+    reg B;
+    wire Y;
+
+    xor_gate uut (
+        .A(A),
+        .B(B),
+        .Y(Y)
+    );
+    initial begin
+        $dumpfile("wave.vcd");
+        $dumpvars(0, xor_gate_tb);
+        $display("A B | Y");
+        A=0; B=0;
+        #10;
+        $display("%b %b | %b",A,B,Y);
+        
+        A=0; B=1;
+        #10;
+        $display("%b %b | %b",A,B,Y);
+
+        A=1; B=0;
+        #10;
+        $display("%b %b | %b",A,B,Y);
+
+        A=1; B=1;
+        #10;
+        $display("%b %b | %b",A,B,Y);
+         
+         $finish;
+    end
+endmodule
